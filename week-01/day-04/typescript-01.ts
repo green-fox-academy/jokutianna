d0a4624a8697bb8cae78a3e1bfec085db7083024
jokutianna <jokutianna@gmail.com>
2020-11-22 11:22:40 +0100

    console.log basics

---
+++ week-01/day-04/typescript-01.ts
@@ -0,0 +1,31 @@
+'use strict'
+
+console.log ('Hello Me')
+
+console.log ('Humpty Dumpty sat on a wall,')
+console.log ('Humpty Dumpty had a great fall,')
+console.log ('All the king\'s horses and all the king\'s men')
+console.log ('Couldn\'t put Humpty together again')
+
+console.log ('Hello Andi!');
+console.log ('Hello Márk!');
+console.log ('Hello Máté!');
+
+console.log ('Hello World!');
+console.log (42);
+console.log (-1);
+console.log (0);
+console.log (123412341234123);
+
+console.log (3.1415);
+console.log (-1.5);
+console.log (0.0);
+console.log (.5);
+console.log (5.);
+
+console.log (1 + 1);
+console.log (5 - 2);
+console.log (3 * 4);
+console.log (6 / 2);
+console.log (5 / 2);
+console.log (5 % 2);
