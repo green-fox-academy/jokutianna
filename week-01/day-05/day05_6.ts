049e3f3901f8c2532a69b28a599f84da202872a9
jokutianna <jokutianna@gmail.com>
2020-11-22 11:45:33 +0100

    Loops beginner

---
+++ week-01/day-05/day05_6.ts
@@ -0,0 +1,21 @@
+'use strict'
+
+// Loops //
+
+let a: number = 0;
+while (a < 10) {
+    console.log (a);
+    a++
+}
+
+for (let i: number = 0; i < 3; i++) {
+    console.log (i);
+}
+
+//Exercises//
+
+let mantra: string ='I won\'t cheat on the exam!';
+console.log (mantra);
+for ( let i: number = 0; i < 100; i++) {
+    console.log (mantra);
+}
