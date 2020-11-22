cd224c08f450f9f3e87c1b9d6cec4b4a6f626bde
jokutianna <jokutianna@gmail.com>
2020-11-22 11:46:14 +0100

    Multiplication table with loop

---
+++ week-01/day-05/day05_7.ts
@@ -0,0 +1,13 @@
+'use strict'
+
+for  (let evenNumbers: number = 1; evenNumbers < 100; evenNumbers + 2) {
+    console.log (evenNumbers);
+}
+
+//Multiplication-table//
+let a: number = 15;
+let b: number;
+for (let c = 1; c <= 10; c++) {
+    b = a * c;
+    console.log (' ' + a + '*' + c + '=' + b);
+}
