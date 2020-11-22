8358e689b3aa42f68af341d059e8a58bbf1d4adf
jokutianna <jokutianna@gmail.com>
2020-11-22 11:27:11 +0100

    console.log types

---
+++ week-01/day-04/typescript-02.ts
@@ -0,0 +1,26 @@
+'use strick'
+
+console.log (true);
+console.log (false);
+console.log (!true);
+console.log (!false);
+
+console.log (true && true);
+console.log (true && false);
+console.log (false && false);
+console.log (false && true);
+
+
+console.log (true || true);
+console.log (true || false);
+console.log (false || false);
+console.log (false || true);
+
+console.log ('apple');
+console.log ('don\'t');
+console.log ('"Everything you can imagine is real." - Picasso');
+
+console.log ('tooth' + 'brush');
+console.log ('My favorite number is: ' + 8);
+
+
