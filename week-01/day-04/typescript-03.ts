8854a2102c06af790a0fd098124e7152454919ab
jokutianna <jokutianna@gmail.com>
2020-11-22 11:28:02 +0100

    console.log with basic math

---
+++ week-01/day-04/typescript-03.ts
@@ -0,0 +1,16 @@
+'use strict';
+
+console.log ('My name is Anna Jókuti and I am 23 years old.');
+console.log ('About me:');
+console.log ('- Height: ' + 180 +'cm' );
+
+
+console.log (13 + 22);
+console.log (22 * 13);
+console.log (22 / 13);
+console.log (22 % 13);
+
+console.log (17 * 5 * 6);
+console.log (17 * 7 * 24)
+
+
