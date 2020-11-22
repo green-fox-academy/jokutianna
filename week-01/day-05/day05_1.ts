83360845d2f5d7f4c51576db3aeb140669a3c496
jokutianna <jokutianna@gmail.com>
2020-11-22 11:41:23 +0100

    ts: swap, bmi, define basic info, variable mutation

---
+++ week-01/day-05/day05_1.ts
@@ -0,0 +1,41 @@
+'use strict';
+
+//Exercise: Swap//
+let numberToSwap1: number = 123;
+let numberToSwap2: number = 526;
+numberToSwap1 = 526;
+numberToSwap2 = 123;
+console.log (numberToSwap1);
+console.log (numberToSwap2);
+
+//Exercise: Bmi//
+let massInKg: number = 81.2;
+let heightInM: number = 1.78;
+console.log ('bmi: ' + massInKg / heightInM)
+
+//Exercise: Define-basic-info//
+let myName: string = 'Anna';
+let myAge: number = 23;
+let myHeight: number = 1.8;
+let amIMarried: boolean = false;
+console.log ('My name: ' + myName);
+console.log ('My age: ' + myAge);
+console.log ('My height: ' + myHeight);
+console.log ('Am I Married? ' + amIMarried);
+ 
+//Variable mutation//
+let a: number = 3;
+console.log (10 + a);
+let b: number = 100;
+console.log (b - 7);
+let c: number = 44;
+console.log (c * 2);
+let d: number = 125;
+console.log (d / 5);
+let e: number = 8;
+console.log (e * e * e);
+
+
+
+
+
