4326a98b3a20eecfb4cf801bcd4bf9e5ce4996d4
jokutianna <jokutianna@gmail.com>
2020-11-23 17:39:47 +0100

    Functions: function return values

---
+++ week-02/day-01/functions03.ts
@@ -0,0 +1,10 @@
+'use strict';
+//Function return values//
+
+function makeGreen(name: string): string {
+    let newName = `Green ${name}`;
+    return newName;
+  }
+  
+  let name = makeGreen('Tojas');
+  greetByName('Whazzup', name);
