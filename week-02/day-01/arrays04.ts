aa5f8d9fe21937b601ec4ee2c9ed8c55d11d34ea
jokutianna <jokutianna@gmail.com>
2020-11-23 16:15:39 +0100

    Arrays: compare lengths

---
+++ week-02/day-01/arrays04.ts
@@ -0,0 +1,11 @@
+'use strict';
+
+//Compare length//
+
+let firstList: number[] = [1, 2, 3];
+let secondList: number[] = [4, 5];
+if (secondList.length > firstList.length) {
+    console.log('p2 is longer');
+} else {
+    console.log('p2 is not longer');
+}
