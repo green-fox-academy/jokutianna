e4a5880645e710f6a3d07125eec4f0fc44238a51
jokutianna <jokutianna@gmail.com>
2020-11-24 15:04:46 +0100

    function: greet

---
+++ week-02/day-02/function-greet.ts
@@ -0,0 +1,10 @@
+'use strict';
+
+let nameToGreet: string = 'Green Fox';
+console.log (nameToGreet);
+
+function greet (name: string) {
+console.log('Greetings,dear ' + name);
+}
+
+greet(nameToGreet);
