c876362f5dbb1029d9e000990d5ab6866a5625db
jokutianna <jokutianna@gmail.com>
2020-11-24 16:58:30 +0100

    Data structures: List introduction 1

---
+++ week-02/day-02/datas-list1.ts
@@ -0,0 +1,14 @@
+'use strict';
+
+let names: string[] = [];
+//console.log(names.length);
+names.push('William');
+//console.log(names.length);
+names.push('John');
+names.push('Amanda');
+//console.log(names.length);
+
+
+for (let i= 0; i < names.length; i++) {
+    console.log(i + 1 + ' ' + names[i]);
+    } 
