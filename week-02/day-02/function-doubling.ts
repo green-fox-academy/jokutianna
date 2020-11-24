283c0a53a37dcc76be2da255cf6f58a4b2df8ab5
jokutianna <jokutianna@gmail.com>
2020-11-24 14:40:18 +0100

    function: doubling

---
+++ week-02/day-02/function-doubling.ts
@@ -0,0 +1,12 @@
+'use strict';
+
+let baseNum: number = 123;
+console.log(baseNum)
+
+function doubling (input: number) :number{
+  console.log(input * 2);
+  return input * 2
+   
+}
+
+doubling(baseNum);
