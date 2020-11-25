73c277af038a6a86b62b146fe5904bd519e4081a
jokutianna <jokutianna@gmail.com>
2020-11-25 14:37:37 +0100

    drawing rectangle and stroke

---
+++ week-02/day-03/drawing-03/drawingexercise3.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// Boilerplate//
+const canvas = document.querySelector('.main-canvas');
+const ctx = canvas.getContext('2d');
+
+ctx.fillStyle = 'green';
+ctx.fillRect(10, 10, 100, 100)
+
+ctx.strokeStyle = 'red';
+ctx.strokeRect(10, 10, 100, 100);
+
+
