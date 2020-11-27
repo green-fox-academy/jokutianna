88bce6e34348c65ab7f39d81884c0ea6d08d1ae7
jokutianna <jokutianna@gmail.com>
2020-11-27 13:17:29 +0100

    colored box

--- week-02/day-03/drawing-03/drawingexercise3.js
+++ week-02/day-03/drawing-03/drawingexercise3.js
@@ -4,10 +4,30 @@
 const canvas = document.querySelector('.main-canvas');
 const ctx = canvas.getContext('2d');
 
-ctx.fillStyle = 'green';
+ctx.fillStyle = 'grey';
 ctx.fillRect(10, 10, 100, 100)
 
-ctx.strokeStyle = 'red';
-ctx.strokeRect(10, 10, 100, 100);
+ctx.strokeStyle ='red';
+ctx.beginPath();
+ctx.moveTo(10, 10);
+ctx.lineTo(10, 110);
+ctx.stroke();
 
+ctx.strokeStyle ='green';
+ctx.beginPath();
+ctx.moveTo(10, 10);
+ctx.lineTo(110, 10);
+ctx.stroke();
+
+ctx.strokeStyle ='blue';
+ctx.beginPath();
+ctx.moveTo(110, 10);
+ctx.lineTo(110, 110);
+ctx.stroke();
+
+ctx.strokeStyle ='yellow';
+ctx.beginPath();
+ctx.moveTo(110, 110);
+ctx.lineTo(10, 110);
+ctx.stroke();
 
