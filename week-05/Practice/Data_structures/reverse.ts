6c32263b2c98fdcf337473f59416600a5c4d54d5
jokutianna <jokutianna@gmail.com>
2020-12-15 11:49:55 +0100

    .split array done

---
+++ week-05/Practice/Data_structures/reverse.ts
@@ -0,0 +1,15 @@
+'use strict';
+// Create a function that can reverse a string, which is passed as the parameter
+// Use it on this reversed string to check it!
+
+
+let toBeReversed: string = `.eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI`;
+
+let toBeReversedArr: string[] = toBeReversed.split("");
+console.log(toBeReversedArr);
+
+
+
+
+//console.log(reverse(toBeReversed));
+//export = reverse;
