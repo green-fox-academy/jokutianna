d4c531d0b6bfbdbb7b51d83af8380222c9e14aa7
jokutianna <jokutianna@gmail.com>
2020-12-31 17:47:01 +0100

    forEach in functions done

--- week-05/Practice/Trial-Exam/Pirate/ship.ts
+++ week-05/Practice/Trial-Exam/Pirate/ship.ts
@@ -16,12 +16,36 @@ export class Ship {
       this._crew.push(pirate);
     } else {
       if (this._hasCaptain  === true) {
-        console.log(`We cab only have a single captain, yarrr!`);
+        console.log(`We can only have a single captain, yarrr!`);
       } else {
         this._crew.push(pirate);
         this._hasCaptain = true;
       }
     }
+  }
+
+  public getGolds(): number {
+    let crewGold: number = 0
+    this._crew.forEach (pirate => {
+      crewGold += pirate.amountOfGold;
+    });
+    return crewGold;
+  }
+
+  public lastDayOnTheShip(): void {
+    this._crew.forEach (pirate => {
+      pirate.party();
+    })
+  }
+
+  //Ezt nem értem (this.lastDayOnTheShip?)
+  public prepareForBattle(): void {
+    this._crew.forEach (pirate => {
+      for (let i: number = 0; i <= 5; i++) {
+        pirate.work();
+      }
+    });
+    this.lastDayOnTheShip();
   }
 
 }
